f7e0135ead8ce6223da2bd5481f30c115834c5f5
GorgonUK <georgekontus@gmail.com>
2020-02-24 02:23:33 +0000

    version 1.1

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,9 +3,9 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button class="gridbox btn-success"  *ngIf="!value"><h1 class="currentVal">{{ value }}</h1></button>
-    <button class="gridbox btn-success"  *ngIf="value == 'X'"><h1 class="xclass">{{ value }}</h1></button>
-    <button class="gridbox btn-danger"  *ngIf="value == 'O'"><h1 class="oclass">{{ value }}</h1></button>
+    <button class="gridbox boxStyle elegant-color-dark"  *ngIf="!value"><h1 class="currentVal">{{ value }}</h1></button>
+    <button class="gridbox btn-danger"  *ngIf="value == 'X'"><h1 class="xclass">{{ value }}</h1></button>
+    <button class="gridbox btn-primary"  *ngIf="value == 'O'"><h1 class="oclass">{{ value }}</h1></button>
   `,
   styleUrls: ['./square.component.sass']
   ,
